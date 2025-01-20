c36330880bf6e08ed9f62aafff60855708749e1f
Dams168 <adamrohman2003@gmail.com>
2025-01-20 19:53:20 +0700

    feat: connect Mongodb,  model seed places

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
+
+//connect to mongoDB
+mongoose.connect('mongodb://127.0.0.1/bestpoints')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.log('Error connecting to MongoDB', err);
+    });
+
+
+//models
+const Place = require('./models/place');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
